c54783777831d6ba6caac9236bb0014220303cf9
sufyan234 <sufyanaslam234@gmail.com>
2020-11-28 10:22:55 -0800

    use_of_assignment_declaration_in_java

---
+++ assignment_statment.java
@@ -0,0 +1,26 @@
+public class assignment_statment{
+    public static void main(String[] args) {
+    
+        // create variables
+        int a = 4;//this is assigining some value to variable a which is 4
+        int var;
+    
+        // assign value using =
+        var = a;//This is variable declaration
+        System.out.println("var using =: " + var);
+    
+        // assign value using =+
+        var += a;//Mean value of variable var + value of  variable a
+        System.out.println("var using +=: " + var);
+    
+        // assign value using =* 
+        var *= a;//variable updated value * value of variable a
+        System.out.println("var using *=: " + var);
+        //use of expression 
+        //This is used to make some expression
+        int result;
+        result = var + a;
+        System.out.println("The sum of values is=: " + result);//answer will be 36
+
+      }
+}
