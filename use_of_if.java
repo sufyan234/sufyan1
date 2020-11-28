2ac5bcbfed47628109f4efe670f201837ee6a839
sufyan234 <sufyanaslam234@gmail.com>
2020-11-28 10:20:35 -0800

    use_of_if_elseif_else

---
+++ use_of_if.java
@@ -0,0 +1,18 @@
+class use_of_if{
+  public static void main(String[] args) {
+    // create a string variable
+    String language = "C++";
+
+    // if statement
+    if (language == "Java") {
+      System.out.println("Best Programming Language");
+    }
+    else if(language == "C++"){
+        System.out.println("Higher level language");
+
+    }
+    else{
+        System.out.println("Something wrong with code");
+    }
+  }
+}
